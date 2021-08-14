3cf1bffa23110c9a1a8d843392898812b048e400
Kristóf Poduszló <kripod@protonmail.com>
2021-08-14 10:56:42 +0200

    chore(deps): update

--- src/index.ts
+++ src/index.ts
@@ -31,9 +31,10 @@ export function cssPropertyAlias(property: string): string | undefined {
 }
 
 export function cssPropertyPrefixFlags(property: string): number {
-	const matches = /^(?:(text-(?:decoration$|e|or|si)|back(?:ground-cl|d|f)|box-d|(?:mask(?:$|-[ispro]|-cl)))|(tab-|column(?!-s)|text-align-l)|(ap)|(u|hy))/i.exec(
-		property,
-	);
+	const matches =
+		/^(?:(text-(?:decoration$|e|or|si)|back(?:ground-cl|d|f)|box-d|(?:mask(?:$|-[ispro]|-cl)))|(tab-|column(?!-s)|text-align-l)|(ap)|(u|hy))/i.exec(
+			property,
+		);
 
 	if (!matches) return 0;
 
@@ -44,9 +45,10 @@ export function cssPropertyPrefixFlags(property: string): number {
 }
 
 export function cssValuePrefixFlags(property: string, value: string): number {
-	const matches = /^(?:(pos)|(background-i)|((?:max-|min-)?(?:block-s|inl|he|widt))|(dis))/i.exec(
-		property,
-	);
+	const matches =
+		/^(?:(pos)|(background-i)|((?:max-|min-)?(?:block-s|inl|he|widt))|(dis))/i.exec(
+			property,
+		);
 
 	if (!matches) return 0;
 
